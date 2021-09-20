563fc331733f1ca79d7fdf2a62e98a34d1ab7ae5
sebastianFinkel <s.finkel@pragmaticindustries.de>
2021-09-20 21:32:35 +0200

    added more files for build process in netlify

---
+++ components/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg class="nuxt-logo" viewBox="0 0 45 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path  d="~/static/logo.svg"/>
+  </svg>
+</template>
+
+<style>
+.nuxt-logo {
+  height: 180px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="theme no-sidebar">
+      <header class="nav-bar">
+        <div class="sidebar-button">
+          <Burger/>
+        </div>
+        <a href="/" class="nav-bar-title">
+          <img src="~/static/logo.svg" alt="Barnbirds Logo" class="logo">
+        </a>
+      </header>
+      <main>
+        <body>
+        <figure>
+          <img src="~/static/logo.svg" class="image" alt="Barnbirds Logo">
+        </figure>
+        <nuxt-link to="/posts/ira">Gesang</nuxt-link>
+        </body>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Burger from '~/components/Burger'
+
+export default {
+  components: { Burger }
+}
+</script>
