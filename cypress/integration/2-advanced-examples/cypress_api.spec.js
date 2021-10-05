2d5b49b2721f74630fdec5d6a210d68134744510
sebastianFinkel <s.finkel@pragmaticindustries.de>
2021-10-05 10:19:33 +0200

    fixed cypress_api.spec.js

--- cypress/integration/2-advanced-examples/cypress_api.spec.js
+++ cypress/integration/2-advanced-examples/cypress_api.spec.js
@@ -92,12 +92,12 @@ context('Cypress.config()', () => {
     let myConfig = Cypress.config()
 
     expect(myConfig).to.have.property('animationDistanceThreshold', 5)
-    expect(myConfig).to.have.property('baseUrl', null)
+    expect(myConfig).to.have.property('baseUrl', 'http://localhost:3000')
     expect(myConfig).to.have.property('defaultCommandTimeout', 4000)
     expect(myConfig).to.have.property('requestTimeout', 5000)
     expect(myConfig).to.have.property('responseTimeout', 30000)
-    expect(myConfig).to.have.property('viewportHeight', 660)
-    expect(myConfig).to.have.property('viewportWidth', 1000)
+    expect(myConfig).to.have.property('viewportHeight', 1400)
+    expect(myConfig).to.have.property('viewportWidth', 1600)
     expect(myConfig).to.have.property('pageLoadTimeout', 60000)
     expect(myConfig).to.have.property('waitForAnimations', true)
 
