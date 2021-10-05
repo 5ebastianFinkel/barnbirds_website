358a77fee1e4b6173b6d736f1b13979e15be1a10
sebastianFinkel <s.finkel@pragmaticindustries.de>
2021-10-05 12:31:06 +0200

    added netlify.toml

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,4 +1,10 @@
 describe('The Home Page', () => {
+  beforeEach( () => {
+    // Visit home site before each test
+    // baseUrl is set in cypress.json to localhost:3000
+    cy.visit('/')
+  })
+
   it('should visit the home page', () => {
     cy.visit('/')
   })
