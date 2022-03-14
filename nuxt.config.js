64687a6560c6ae4f7b46fde97c5d1d1f55fd6598
Sebastian Finkel <s.finkel@posteo.de>
2022-03-14 21:04:20 +0100

    added google font

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/google-fonts',
     '@nuxtjs/eslint-module',
     '@nuxtjs/stylelint-module',
     '@nuxtjs/fontawesome',
